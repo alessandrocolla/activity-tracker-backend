4482000663f9acb1a61a67143ad13dac86d0dfa0
Piero Carimi <pierocarimi@gmail.com>
2024-05-15 16:41:57 +0200

    feat(model): added task schema in taskModel.js file

---
+++ models/taskModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  taskName: {
+    type: String,
+    required: [true, "Please enter the task name"],
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  state: {
+    type: String,
+    enum: {
+      values: ["To do", "In progress", "Done"],
+      message: "State is either: 'To do', 'In progress', 'Done'",
+    },
+    default: "To do",
+  },
+  progressState: {
+    type: Number,
+    default: 0,
+    min: [0, "Progress state must be above or equal to 0"],
+    max: [100, "Progress state must be below or equal to 100"],
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
