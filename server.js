189cd3f13e1594ceb309410e3050e81620a284ff
alessandro.colla <ale03colla@gmail.com>
2024-05-15 15:24:55 +0200

    feat/database connection and prettier rules

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
 
 const port = process.env.PORT || 3000;
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then(() => {
+  console.log("Connected to DB successfully");
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
