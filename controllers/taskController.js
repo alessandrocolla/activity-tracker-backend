4dfc41f09da7814ba54f643ae37e2c7de82e3b5d
Alessandro Colla <ale03colla@gmail.com>
2024-05-16 17:40:41 +0200

    feat(api): added task get route and controller

---
+++ controllers/taskController.js
@@ -0,0 +1,18 @@
+const Task = require("../models/taskModel");
+
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+
+    res.status(200).json({
+      status: "success",
+      results: tasks.length,
+      data: tasks,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
