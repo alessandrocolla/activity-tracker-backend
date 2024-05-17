cf1be726245041f16c16d4fb0f0377e23743e42d
Accia <stefanocammarata@outlook.it>
2024-05-17 14:29:48 +0200

    feat(api): added POST createActivity method

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -26,7 +26,30 @@ exports.getActivity = async (req, res) => {
       data: activity,
     });
   } catch (err) {
-    res.status(404).json({
+    res.status(404).json({ status: "fail", message: err });
+  }
+};
+
+exports.createActivity = async (req, res) => {
+  try {
+    const newActivity = await Activity.create({
+      taskName: req.body.taskName,
+      taskID: req.body.taskID,
+      activityDate: req.body.activityDate,
+      startTime: req.body.startTime,
+      endTime: req.body.endTime,
+      notes: req.body.notes,
+      userID: req.body.userID,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        activity: newActivity,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
       status: "fail",
       message: err,
     });
