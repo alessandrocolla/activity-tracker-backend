cde613b2948d6792cb732b8dadae7558f920e955
Accia <stefanocammarata@outlook.it>
2024-05-17 10:40:37 +0200

    feat(api): fixed app.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const taskRouter = require("./routes/taskRoutes");
 
 const app = express();
 
-const taskRouter = require("./routes/taskRoutes");
-
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
