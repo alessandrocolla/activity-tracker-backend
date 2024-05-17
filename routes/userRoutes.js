0e3b8e15b47c0244924eb669aea0e55de14fd65b
Alberto Cangialosi <billyballo.res@gmail.com>
2024-05-17 10:56:54 +0200

    feat(api): revision, finished POST /users/login
    add instance method `correctPassword`
    
    password not yet encrypted

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
+router.post("/login", authController.login);
 router.route("/").post(userController.createUser);
 
 module.exports = router;
