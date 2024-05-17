b17bdc51c689f9d77009b4068028143f7ef0edea
Accia <stefanocammarata@outlook.it>
2024-05-17 17:33:15 +0200

    feat(api): added get, delete and patch methods for Task

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,5 +4,6 @@ const taskController = require("../controllers/taskController");
 const router = express.Router();
 
 router.route("/").get(taskController.getTasks).post(taskController.createTask);
+router.route("/:id").get(taskController.getTask).delete(taskController.deleteTask).patch(taskController.updateTask);
 
 module.exports = router;
