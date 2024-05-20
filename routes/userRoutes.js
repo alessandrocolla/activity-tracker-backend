b23e01620ffcc24865a15fab03a29da34c863c2e
Piero Carimi <pierocarimi@gmail.com>
2024-05-20 12:15:32 +0200

    feat(api): added PATCH resetPassword  method

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
 
 router.route("/").get(userController.getUsers).post(userController.createUser);
 router.route("/:id").get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
