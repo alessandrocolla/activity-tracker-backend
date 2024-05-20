6da299d5ede9d907e1d928c3cdd7478ad8d408c9
Piero Carimi <pierocarimi@gmail.com>
2024-05-20 15:58:31 +0200

    feat(middleware): added middleware to set passwordChangedAt on password change

--- models/userModel.js
+++ models/userModel.js
@@ -78,6 +78,13 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
