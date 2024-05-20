edda997b23fca76db6d555a8bd4e83d801323610
alessandro.colla <ale03colla@gmail.com>
2024-05-20 12:00:44 +0200

    fix(api)/added catchAsync method to apis

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,94 +1,71 @@
 const Task = require("../models/taskModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
 
-exports.getTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find();
+exports.getTasks = catchAsync(async (req, res, next) => {
+  const tasks = await Task.find();
 
-    res.status(200).json({
-      status: "success",
-      results: tasks.length,
-      data: {
-        tasks,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({ status: "fail", message: err });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    results: tasks.length,
+    data: {
+      tasks,
+    },
+  });
+});
 
-exports.getTask = async (req, res) => {
-  try {
-    const task = await Task.findById(req.params.id);
+exports.getTask = catchAsync(async (req, res, next) => {
+  const task = await Task.findById(req.params.id);
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        Tasks: task,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  if (!task) return next(new AppError("Task not found.", 404));
 
-exports.createTask = async (req, res) => {
-  try {
-    const newTask = await Task.create({
-      taskName: req.body.taskName,
-      isActive: req.body.isActive,
-      state: req.body.state,
-      progressState: req.body.progressState,
-    });
+  res.status(200).json({
+    status: "success",
+    data: {
+      Tasks: task,
+    },
+  });
+});
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        task: newTask,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+exports.createTask = catchAsync(async (req, res, next) => {
+  const newTask = await Task.create({
+    taskName: req.body.taskName,
+    isActive: req.body.isActive,
+    state: req.body.state,
+    progressState: req.body.progressState,
+  });
 
-exports.updateTask = async (req, res) => {
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: "success",
-      data: {
-        task,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  res.status(201).json({
+    status: "success",
+    data: {
+      task: newTask,
+    },
+  });
+});
 
-exports.deleteTask = async (req, res) => {
-  try {
-    await Task.findByIdAndDelete(req.params.id);
+exports.updateTask = catchAsync(async (req, res, next) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    res.status(204).json({
-      status: "success",
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  if (!task) return next(new AppError("Task not found.", 404));
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      task,
+    },
+  });
+});
+
+exports.deleteTask = catchAsync(async (req, res, next) => {
+  const task = await Task.findByIdAndDelete(req.params.id);
+
+  if (!task) return next(new AppError("Task not found.", 404));
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
