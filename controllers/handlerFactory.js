cd7ccee0b307bc7244a6fe4475b24c3bf678295b
Piero Carimi <pierocarimi@gmail.com>
2024-05-21 17:38:50 +0200

    feat(features): added the APIFeatures class in the apiFeatures.js file and modified the getAll function in the handlerFactory.js file

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,9 +1,19 @@
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.getAll = (Model) =>
   catchAsync(async (req, res, next) => {
-    const document = await Model.find();
+    let filter = {};
+    if (req.params.activityDate) filter = { activityDate: req.params.activityDate };
+    if (req.params.taskName) filter = { task: req.params.taskName };
+    if (req.params.firstName) filter = { firstName: req.params.firstName };
+    if (req.params.lastName) filter = { lastName: req.params.lastName };
+    if (req.params._id) filter = { _id: req.params._id };
+
+    const features = new APIFeatures(Model.find(filter), req.query).filter().sort().limitFields().paginate();
+
+    const document = await features.query;
 
     res.status(200).json({
       status: "success",
