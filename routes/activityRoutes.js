2cf5d5ce1be9a0ac274481e3d2937639b13f23d3
Piero Carimi <pierocarimi@gmail.com>
2024-05-23 15:49:43 +0200

    feat(middleware): added restrictToOwnerOrAdmin middleware to the authController.js file

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const activityController = require("../controllers/activityController");
 const authController = require("../controllers/authController");
+const { restrictToOwnerOrAdmin } = require("../controllers/authController");
+const Activity = require("../models/activityModel");
 
 const router = express.Router();
 
@@ -10,8 +12,8 @@ router
   .post(authController.protectRoute, activityController.createActivity);
 router
   .route("/:id")
-  .get(authController.protectRoute, activityController.getActivity)
-  .patch(authController.protectRoute, activityController.updateActivity)
-  .delete(authController.protectRoute, activityController.deleteActivity);
+  .get(authController.protectRoute, restrictToOwnerOrAdmin(Activity), activityController.getActivity)
+  .patch(authController.protectRoute, restrictToOwnerOrAdmin(Activity), activityController.updateActivity)
+  .delete(authController.protectRoute, restrictToOwnerOrAdmin(Activity), activityController.deleteActivity);
 
 module.exports = router;
