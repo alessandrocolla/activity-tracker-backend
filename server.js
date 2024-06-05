79202fcaeeefb8cd5639056f46b8c4972d32a6d2
Alberto Cangialosi <billyballo.res@gmail.com>
2024-06-05 15:23:04 +0200

    fix(app): `app` imported after `dotenv.config()`

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
 
 const port = process.env.PORT || 4200;
 
