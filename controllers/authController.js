c45eec1b89d8ecbec302d0870653332e7449262b
Piero Carimi <pierocarimi@gmail.com>
2024-06-07 13:03:14 +0200

    fix(cookie): change course options and cookie options

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,6 +20,7 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: "Lax",
   };
   // Add this line when production is managed
   // if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
