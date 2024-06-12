fa2ed0c9281da6c85dacb7e73116a453d3efab80
Piero Carimi <pierocarimi@gmail.com>
2024-06-12 14:10:33 +0200

    fix(limit): removed the default limit of 25 in pagination in the apiFeatures.js file

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -40,7 +40,7 @@ class APIFeatures {
 
   paginate() {
     const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 25;
+    const limit = this.queryString.limit * 1;
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
