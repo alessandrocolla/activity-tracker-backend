e96a92d1d003c52d8c7acf3ddcab3d725b284894
alessandro.colla <ale03colla@gmail.com>
2024-06-25 10:45:53 +0200

    fix(prettier): applied prettier rules to activityController

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -47,7 +47,11 @@ exports.personalActivities = catchAsync(async (req, res, next) => {
 
   const totalDocuments = await Activity.countDocuments(filter);
 
-  const features = new APIFeatures(Activity.find(filter), req.query, "Activity").filter().sort().limitFields().paginate();
+  const features = new APIFeatures(Activity.find(filter), req.query, "Activity")
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
   const userActivities = await features.query;
 
