0a7817d95c1edf01ac2e3c60295f3fa7a6d4c973
Alessandro Colla <ale03colla@gmail.com>
2024-07-08 10:12:00 +0200

    fix(limit): increased requests per hour to meet frontend needs

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 const limiter = rateLimit({
-  max: 1000,
+  max: 100000,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
 });
