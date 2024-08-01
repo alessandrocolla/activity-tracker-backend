3ec4d36dfbd8e855f2eefdb0b2c3af6076ae812f
gian <gianmarco.clu23@gmail.com>
2024-08-01 15:38:10 +0200

    feat(captcha): implemented a controller to control if a captcha is valid

---
+++ controllers/captchaController.js
@@ -0,0 +1,47 @@
+const https = require("https");
+const querystring = require("querystring");
+const AppError = require("../utils/appError");
+
+const verifyCaptcha = (req, res) => {
+  const token = req.body["g-recaptcha-response"];
+  const secretKey = "6LeBbRsqAAAAAF6y0tHCVBM3n_UP3r2_v1QueIen";
+
+  const postData = querystring.stringify({
+    secret: secretKey,
+    response: token,
+  });
+
+  const options = {
+    hostname: "www.google.com",
+    path: "/recaptcha/api/siteverify",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Content-Length": postData.length,
+    },
+  };
+
+  const request = https.request(options, (response) => {
+    let data = "";
+
+    response.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    response.on("end", () => {
+      const result = JSON.parse(data);
+      if (!result.success) {
+        return (new AppError("Internal server error.", 500));
+      }
+    });
+  });
+
+  request.on("error", (error) => {
+    res.status(500).send("Internal server error.");
+  });
+
+  request.write(postData);
+  request.end();
+};
+
+module.exports = verifyCaptcha;
